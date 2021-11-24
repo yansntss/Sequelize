0774a5203523cef9e954db006a0bf76808385f0e
yansntss <yansantos.dev@gmail.com>
2021-11-24 19:55:21 -0300

    configura db

---
+++ config/config.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+module.export=
+{
+  "development": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PWD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": process.env.DB_DIALECT,
+    "logging": true //aparecer o resultado no terminal
+  },
+  "test": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PWD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": process.env.DB_DIALECT,
+    "logging": true 
+  },
+  "production": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PWD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": process.env.DB_DIALECT,
+    "logging": false 
+  }
+}
